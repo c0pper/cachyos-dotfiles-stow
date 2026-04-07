b1ca8eca1a23cda2d65863b34f8f8cee24849fa5
simo <93simonster@gmail.com>
2026-04-07 20:03:02 +0200

    yazi move to directory on quit and config file added

--- .mydotfiles/com.ml4w.dotfiles/.config/fish/conf.d/20-customization.fish
+++ .mydotfiles/com.ml4w.dotfiles/.config/fish/conf.d/20-customization.fish
@@ -7,3 +7,12 @@
 # -----------------------------------------------------
 eval "$($HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/ohmyposh/zen.toml)"
 # eval "$($HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/ohmyposh/EDM115-newline.omp.json)"
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
