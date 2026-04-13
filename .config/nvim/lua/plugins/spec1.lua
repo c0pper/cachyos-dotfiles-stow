8a3856d1750f5e3e84c3edb564a6017875089722
simo <93simonster@gmail.com>
2026-04-13 17:24:42 +0200

    nvim tree

--- .config/nvim/lua/plugins/spec1.lua
+++ .config/nvim/lua/plugins/spec1.lua
@@ -80,7 +80,18 @@ return {
         { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
         { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
       },
-    }
+    },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  }
 }
 
 
